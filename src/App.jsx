0863b9acc3f92e3a0945e99197712ed4edc1d8d7
MateoTonkovic <mateo@houlak.com>
2024-09-16 20:49:43 -0300

    Made some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
-import Oulet1 from "./pages/Oulets/Oulet1";
-import Oulet2 from "./pages/Oulets/Oulet2";
 import OutletExample from "./pages/OutletExample";
+import Oulet1 from "./pages/Outlets/Outlet1";
+import Oulet2 from "./pages/Outlets/Outlet2";
+import PrivateRoutes from "./pages/PrivateRoutes";
 import UseLocationExample from "./pages/UseLocation";
 import UseParamsExample from "./pages/UseParamsExample";
 import WhatIsReactRouter from "./pages/WhatIsReactRouter";
@@ -11,6 +12,7 @@ import WhyReactRouter from "./pages/WhyReactRouter";
 
 function App() {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
   return (
     <div className="app-container">
@@ -26,9 +28,14 @@ function App() {
               ¿React Router?
             </Link>
           </li>
-          <li className="nav-item">
+          {/* <li className="nav-item">
             <Link to="/why-use-react-router" className="nav-link">
               ¿usar React Router?
+            </Link>
+          </li> */}
+          <li className="nav-item">
+            <Link to="/private" className="nav-link">
+              Ruta privada
             </Link>
           </li>
           <li className="nav-item">
@@ -65,6 +72,14 @@ function App() {
               atras
             </button>
           </li>
+          <li>
+            <button
+              onClick={() => setIsLoggedIn((prev) => !prev)}
+              className="nav-button"
+            >
+              {!isLoggedIn ? "loguear usuario" : "desloguear usuario"}
+            </button>
+          </li>
         </ul>
       </nav>
 
@@ -85,6 +100,9 @@ function App() {
             <Route path="outlet-1" element={<Oulet1 />} />
             <Route path="outlet-2" element={<Oulet2 />} />
             <Route path="*" element={<h1>Error 404 en Oulet</h1>} />
+          </Route>
+          <Route element={<PrivateRoutes isLoggedIn={isLoggedIn} />}>
+            <Route path="private" element={<h1>Esta es una ruta privada</h1>} />
           </Route>
           <Route path="*" element={<h1>Error 404</h1>} />
         </Routes>
